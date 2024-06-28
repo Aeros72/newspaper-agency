81a0f50f7aebd6fa938532c5ada78ea6cc37f415
Aeros72 <pashamelnyk2004@gmail.com>
2024-06-28 16:43:49 +0300

    defined login.html and logged_out.html

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'newspaper'
 ]
 
