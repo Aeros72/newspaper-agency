a4242461c503662a0c820c5b5ad525a93f16aa31
Aeros72 <pashamelnyk2004@gmail.com>
2024-06-28 16:20:10 +0300

    defined index.html and base.html

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -108,7 +108,7 @@ AUTH_USER_MODEL = 'newspaper.Redactor'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
